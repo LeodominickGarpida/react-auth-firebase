f11e12e1e7e6b956cc9df09690238f917aad544e
Dominick Garpida <leodominickgarpida@yahoo.com>
2022-06-06 23:22:24 +0800

    Add redirect in Login and change password

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,9 +1,11 @@
 import { useRef, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import AuthContext from '../../store/auth-context';
 
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const history = useHistory()
   const newPasswordInputRef = useRef()
   const { token } = useContext(AuthContext)
 
@@ -25,7 +27,9 @@ const ProfileForm = () => {
         }),
         headers: { 'Content-Type': 'application/json' }
     }).then(res => {
-      console.log('Success')
+      // console.log('Success')
+
+      history.replace('/')
     })
   }
 
